9846b7a5b31d42dd3b9701aa46ce69860505439d
Ejbert <ebertelsen2015@kellogg.northwestern.edu>
2015-02-12 23:00:47 -0600

    Week 5 hw

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,23 +6,38 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find_by(id: params["id"])
-    @review = []
-    
+    @reviews = Review.where(restaurant_id: @restaurant.id)
+    @users = []
+    @reviews.each do |review|
+      @users << User.find_by(id: review.user_id)
+    end
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    restaurant_params = params.require(:restaurant).permit!
+    Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
   end
 
   def edit
+    @restaurant = Restaurant.find_by(id: params["id"])
   end
 
   def update
+    restaurant_params = params.require(:restaurant).permit!
+    @restaurant = Restaurant.find_by(id: params["id"])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path
   end
 
   def destroy
+    @restaurant = Restaurant.find_by(id: params["id"])
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
 end
